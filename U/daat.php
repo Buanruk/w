cb9c0724dbb54ba17ba4896feaeb6ccb8e5c6c18
Buanruk <garfiled.peerapat@gmail.com>
2024-10-17 15:17:09 +0700

    a

--- U/daat.php
+++ U/daat.php
@@ -8,5 +8,4 @@ $dbName = "shop1";
 $conn = mysqli_connect($host, $usr, $pwd , $dbName) or die ("เชื่อมต่อฐานข้อมูลไม่ได้") ;
 mysqli_select_db($conn, $dbName) ;
 mysqli_query($conn, "set names utf8");
-
 ?>
